5efb4320351b72931d7e6acfad8ed3819310f183
Jordan Gomes <me@jordangomes.com>
2024-04-28 22:22:14 +1000

    Fix phase 2 script runner path

--- AutoPilot/Scripts/CopyPhase2.ps1
+++ AutoPilot/Scripts/CopyPhase2.ps1
@@ -13,7 +13,7 @@ New-Item -ItemType Directory -Path "C:\Windows\Panther\Unattend" -Force -ErrorAc
 try {
     Write-Host "Copying Unattend file to System"
     $contents = Get-Content "$PSScriptRoot\..\unattendphases\phase2.xml"
-    $script = "powershell -NoProfile -ExecutionPolicy Bypass -Command `"67..90|%{[Char]`$PSItem}|%{if(Test-Path `$(`$_+':\AutoPilot\Scripts\run.ps1')){&`$(`$_+':\AutoPilot\run.ps1') -ProfileName '$name' -Phase '2'}}`"" 
+    $script = "powershell -NoProfile -ExecutionPolicy Bypass -Command `"67..90|%{[Char]`$PSItem}|%{if(Test-Path `$(`$_+':\AutoPilot\Scripts\run.ps1')){&`$(`$_+':\AutoPilot\Scripts\run.ps1') -ProfileName '$name' -Phase '2'}}`"" 
     $newContents = $contents.Replace("{{CollectAPHash}}", $script)
     $newContents -join "`r`n" | Out-File -FilePath "C:\Windows\Panther\Unattend\Unattend.xml" -NoNewline -Encoding "UTF8" -Force
 } catch {
